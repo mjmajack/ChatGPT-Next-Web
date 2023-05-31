c0bfdad09f69ce2419dd4d6fa259500085bc5820
mjmajack <134709090+mjmajack@users.noreply.github.com>
2023-05-31 22:53:55 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>为更好体验，请添加微信DaAi0991</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
